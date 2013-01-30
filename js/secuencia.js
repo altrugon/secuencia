f7842e2dc74ad2e59e75d089ae7098196e415aad
Alberto Trujillo Gonzalez <altrugon@gmail.com>
2013-01-30 15:15:54 -0800

    Rename to "Secuencia".

--- js/secuencia.js
+++ js/secuencia.js
@@ -14,7 +14,7 @@
  */
 
 // call it like this for horizontal dragging animation, in your Ready event:
-//     new RotateWidget('myImgTagID', 'myImgTagID', 'myImgTagID', callback(i), 32);  // callback generates file URL
+//     new Secuencia('myImgTagID', 'myImgTagID', 'myImgTagID', callback(i), 32);  // callback generates file URL
 // your <img has id myImgTagID, and it's initial src is the 'key frame',
 //    = whats shown before the user starts playing wih it (prob frame 0).
 // Your thirty two image frames can be named anything that your callback can generate given
@@ -26,27 +26,27 @@
 // dragAreaId = user can drag/animate over this area, same or parent of clickDownId, maybe body
 //
 // call it like this for vertical dragging animation:
-//     new RotateWidget('myImgTagID', callback(i, j), 1, 6);
+//     new Secuencia('myImgTagID', callback(i, j), 1, 6);
 // Your six image frames are indexed by j
 // call it like this for both:
-//     new RotateWidget('myImgTagID', callback(i, j), 5, 5);
+//     new Secuencia('myImgTagID', callback(i, j), 5, 5);
 // Your 25 image frames are indexed by i and j
 // Don't worry about the object created; all the events will find it, you don't need it.  just construct it.
-function RotateWidget(imgId, clickDownId, dragAreaId, frameURLPat, nFramesX, nFramesY, options) {
+function Secuencia(imgId, clickDownId, dragAreaId, frameURLPat, nFramesX, nFramesY, options) {
     this.options = options || {};
     this.imgjq = this.elem('image_el', $('#' + imgId)) ;
-    this.imgjq[0].rotateWidget = this;  // how we find this upon events.
+    this.imgjq[0].secuencia = this;  // how we find this upon events.
 
     this.clickjq = this.elem('click_el', $('#' + clickDownId) || this.imgjq);
-    this.clickjq.mousedown(RotateWidget.down);
-    this.clickjq[0].rotateWidget = this;
+    this.clickjq.mousedown(Secuencia.down);
+    this.clickjq[0].secuencia = this;
     this.clickjq.css('cursor', 'move');
 
     this.dragjq = this.elem('drag_el', $('#' + dragAreaId) || this.clickjq);
-    this.dragjq.mousemove(RotateWidget.move);
-    this.dragjq.mouseup(RotateWidget.up);
-    this.dragjq.mouseout(RotateWidget.out);
-    this.dragjq[0].rotateWidget = this;
+    this.dragjq.mousemove(Secuencia.move);
+    this.dragjq.mouseup(Secuencia.up);
+    this.dragjq.mouseout(Secuencia.out);
+    this.dragjq[0].secuencia = this;
 
     this.curFrameX = this.newFrameX = this.lastFrameX = this.opt('start_x', 0);
     this.curFrameY = this.newFrameY = this.lastFrameY = this.opt('start_y', 0);
@@ -66,14 +66,14 @@ function RotateWidget(imgId, clickDownId, dragAreaId, frameURLPat, nFramesX, nFr
     // touch events only for iOS (ipad) - they disabled move events while the mouse is down in the new iOS.
     // kindof kills dragging.  and they gave us pageX & Y on the event so us retro people with only a primitive mouse
     // can pretend it's just a mouse event.
-    this.clickjq.bind('touchstart', RotateWidget.down);
-    this.dragjq.bind('touchmove', RotateWidget.move);
-    this.dragjq.bind('touchend', RotateWidget.up);
-    this.dragjq.bind('touchcancel', RotateWidget.out);
+    this.clickjq.bind('touchstart', Secuencia.down);
+    this.dragjq.bind('touchmove', Secuencia.move);
+    this.dragjq.bind('touchend', Secuencia.up);
+    this.dragjq.bind('touchcancel', Secuencia.out);
 }
 
 // return jquery-ized version of the opt_name option, or the default.  option is either a string ID, or an element
-RotateWidget.prototype.opt = function opt(opt_name, def) {
+Secuencia.prototype.opt = function opt(opt_name, def) {
     if (null == this.options[opt_name])
         return def;
     else
@@ -81,7 +81,7 @@ RotateWidget.prototype.opt = function opt(opt_name, def) {
 }
 
 // return jquery-ized version of the opt_name option, or the default.  option is either a string ID, or an element
-RotateWidget.prototype.elem = function go(opt_name, def) {
+Secuencia.prototype.elem = function go(opt_name, def) {
     if (null == this.options[opt_name])
         return def;
     var el = this.options[opt_name];
@@ -96,7 +96,7 @@ RotateWidget.prototype.elem = function go(opt_name, def) {
 // if it's too early, failover to a good guess given our tendency toward large graphics.
 // all that's wrong is the speed of rotation is off by prob within a factor of 2
 // go ahead and call this again if you want after the image has loaded.
-RotateWidget.prototype.getSizeInfo = function gsi() {
+Secuencia.prototype.getSizeInfo = function gsi() {
     this.width = this.imgjq.width();
     this.xPerFrame = (this.width || 600) / this.nX;
     this.height = this.imgjq.height();
@@ -111,7 +111,7 @@ RotateWidget.prototype.getSizeInfo = function gsi() {
 
 // part of initialization, but do it slowly.  Preload the images for faster animation (ultimately).
 // Start at the starting position and move outward; those are prob the first frames needed.
-RotateWidget.prototype.preloadAFew = function gsi() {
+Secuencia.prototype.preloadAFew = function gsi() {
     if (this.frameToPreloadY >= this.halfY)
         return;  // done!
 
@@ -140,21 +140,21 @@ RotateWidget.prototype.preloadAFew = function gsi() {
 
 // mouse down: remember this point, and that the mouse is down.
 // This is a static method of the class, installed as a jQ event handler, so this is probably the img
-RotateWidget.down = function dn(event) {
+Secuencia.down = function dn(event) {
     // if touchstart, move, etc. re-assign X+Y values
     if(event.originalEvent.touches && event.originalEvent.touches.length) {
         event.pageX = event.originalEvent.targetTouches[0].pageX;
         event.pageY = event.originalEvent.targetTouches[0].pageY;
     }
 
-    var rw = (event.currentTarget || event.target).rotateWidget;
+    var rw = (event.currentTarget || event.target).secuencia;
     if (!rw) return;  // sometimes with IE
 
     // if drag regions overlap, use the one that got the clickdown event.
-    RotateWidget.current = rw;
+    Secuencia.current = rw;
 
-    RotateWidget.downX = event.pageX;
-    RotateWidget.downY = event.pageY;
+    Secuencia.downX = event.pageX;
+    Secuencia.downY = event.pageY;
 
     // often click makes a textual selection that wants to be dragged.  stop that.
     event.preventDefault();
@@ -162,30 +162,30 @@ RotateWidget.down = function dn(event) {
 }
 
 // up or out: remember mouse is now up, and how far they dragged.
-RotateWidget.up = function up(event) {
-    var rw = RotateWidget.current;
+Secuencia.up = function up(event) {
+    var rw = Secuencia.current;
     if (!rw) return;
 
     rw.curFrameX = rw.newFrameX;
     rw.curFrameY = rw.newFrameY;
-    RotateWidget.current = null;
+    Secuencia.current = null;
 
     event.preventDefault();
     event.stopPropagation();
 }
 
 // oh wait we also get mouseouts when user drags over a child node.. don't freak
-RotateWidget.out = function up(event) {
+Secuencia.out = function up(event) {
     // see if the interfering node is a child of ours.  If so, don't stop dragging.
     for (var n = event.relatedTarget; n && n != document; n = n.parentNode)
         if (n == (event.currentTarget || event.target))
             return;
 
-    RotateWidget.up(event)
+    Secuencia.up(event)
 }
 
 // actually animate, one step at a time.
-RotateWidget.move = function mv(event) {
+Secuencia.move = function mv(event) {
     // if we preload only upon move events (only when mouse over our widget), they will only happen for users
     //  who might try to drag.  Not for people who immediately scroll to the bottom and never use it.
     // Also, loads happen gradually, not all at once.
@@ -197,22 +197,22 @@ RotateWidget.move = function mv(event) {
         event.pageY = event.originalEvent.targetTouches[0].pageY;
     }
 
-    var hovering = (event.currentTarget || event.target).rotateWidget;
+    var hovering = (event.currentTarget || event.target).secuencia;
     if (hovering) hovering.preloadAFew();
 
-    var rw = RotateWidget.current;
+    var rw = Secuencia.current;
     if (!rw)
         return;  // not in a drag
 
     // how far, in frames, has user dragged so far
-    var dx = event.pageX - RotateWidget.downX;  // how many pixels user has dragged
+    var dx = event.pageX - Secuencia.downX;  // how many pixels user has dragged
     rw.newFrameX = rw.curFrameX + Math.round(dx / rw.xPerFrame);
     if (rw.wrap_around)
         rw.newFrameX = rw.newFrameX - Math.floor(rw.newFrameX / rw.nX) * rw.nX;  // modulus nframes
     else
         rw.newFrameX = rw.newFrameX < 0 ? 0 : rw.newFrameX >= rw.nX ? rw.nX-1 : rw.newFrameX;  // pin to ends
 
-    var dy = event.pageY - RotateWidget.downY;
+    var dy = event.pageY - Secuencia.downY;
     rw.newFrameY = rw.curFrameY + Math.round(dy / rw.yPerFrame);
     if (rw.wrap_around)
         rw.newFrameY = rw.newFrameY - Math.floor(rw.newFrameY / rw.nY) * rw.nY;
